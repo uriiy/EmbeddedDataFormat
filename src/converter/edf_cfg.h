5f62589df9701a19166d2d6e93686241e89a94d5
SAVPC\Sav <alex3696@yandex.ru>
2025-08-14 14:38:07 +0700

    FIX/Proj - check and fix UTF8 support

--- src/converter/edf_cfg.h
+++ src/converter/edf_cfg.h
@@ -12,7 +12,12 @@ extern "C" {
 
 //#define STREAM_BUF_SIZE 64
 
-
+// !!! use UTF-8 (no BOM) as source files in MSVC strlen
+// check:	assert(8 == strlen("тест"));
+// https://learn.microsoft.com/en-us/cpp/build/reference/utf-8-set-source-and-executable-character-sets-to-utf-8?view=msvc-170
+// https://habr.com/ru/articles/731614/
+// https://stackoverflow.com/questions/58580912/msvc-utf8-string-encoding-uses-incorrect-code-points
+// https://stackoverflow.com/questions/1660712/specification-of-source-charset-encoding-in-msvc-like-gcc-finput-charset-ch
 
 
 //-----------------------------------------------------------------------------
